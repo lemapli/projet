9d5faa2a15239e18396ac6036fa73c22744dad41
Charlessnop10 <119662329+Charlessnop10@users.noreply.github.com>
2024-12-22 13:59:21 +0100

    Update move.js
    Améliorations et ajouts
    Validation pour la colonne (entre 0 et 6).
    Possibilité de faire une association avec User si on souhaite retrouver l’utilisateur qui a joué ce coup.

--- Puissance4-API/models/move.js
+++ Puissance4-API/models/move.js
@@ -1,15 +1,27 @@
 module.exports = (sequelize, DataTypes) => {
-    const Move = sequelize.define('Move', {
-      playerId: {
-        type: DataTypes.INTEGER,
-        allowNull: false
-      },
-      column: {
-        type: DataTypes.INTEGER,
-        allowNull: false
+  const Move = sequelize.define('Move', {
+    playerId: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    column: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      validate: {
+
+        min: 0,
+        max: 6
       }
-    });
-  
-    return Move;
+    }
+  }, {
+    tableName: 'moves',
+    timestamps: true
+  });
+
+  Move.associate = (models) => {
+    Move.belongsTo(models.Game, { foreignKey: 'gameId' });
+
   };
-  
+
+  return Move;
+};
