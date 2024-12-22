eb5366c9a9b8945791bd4c9629ec1323f083726c
Charlessnop10 <119662329+Charlessnop10@users.noreply.github.com>
2024-12-22 14:00:22 +0100

    Update user.js
    Améliorations et ajouts
    Validation sur username (longueur) et email (format).
    Hooks pour hasher le mot de passe en amont (beforeCreate, beforeUpdate).
    Méthode d’instance pour comparer les mots de passe.

--- Puissance4-API/models/user.js
+++ Puissance4-API/models/user.js
@@ -1,26 +1,64 @@
+const bcrypt = require('bcrypt');
+
 module.exports = (sequelize, DataTypes) => {
-    const User = sequelize.define('User', {
-      username: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        unique: true
-      },
-      email: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        unique: true
-      },
-      password: {
-        type: DataTypes.STRING,
-        allowNull: false
-      },
-      role: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        defaultValue: 'user'
+  const User = sequelize.define('User', {
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        len: {
+          args: [3, 50],
+          msg: 'Le nom d’utilisateur doit contenir entre 3 et 50 caractères.'
+        }
       }
-    });
-  
-    return User;
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        isEmail: {
+          msg: 'Le format de l’email est invalide.'
+        }
+      }
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    role: {
+      type: DataTypes.ENUM('user', 'admin'),
+      allowNull: false,
+      defaultValue: 'user'
+    }
+  }, {
+    tableName: 'users',
+    timestamps: true,
+    hooks: {
+      beforeCreate: async (user, options) => {
+        if (user.password) {
+          const salt = await bcrypt.genSalt(10);
+          user.password = await bcrypt.hash(user.password, salt);
+        }
+      },
+      beforeUpdate: async (user, options) => {
+        // Si jamais l’utilisateur modifie son mot de passe
+        if (user.changed('password')) {
+          const salt = await bcrypt.genSalt(10);
+          user.password = await bcrypt.hash(user.password, salt);
+        }
+      }
+    }
+  });
+
+  User.associate = (models) => {
+
   };
-  
+
+  User.prototype.comparePassword = async function(password) {
+    return await bcrypt.compare(password, this.password);
+  };
+
+  return User;
+};
